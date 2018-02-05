80739ba5fbf0c0af6de529cbcb42973c3a01ef2c
Christoph Klocker <christoph@vedanova.com>
2018-02-05 10:32:23 +0100

    implemented doctest

--- lib/dornbirnfurtbot.ex
+++ lib/dornbirnfurtbot.ex
@@ -1,18 +1,40 @@
 defmodule Dornbirnfurtbot do
+  require Logger
+
   @moduledoc """
   Documentation for Dornbirnfurtbot.
   """
 
   @doc """
-  Hello world.
+  Start the bot app as a new application
 
   ## Examples
 
-      iex> Dornbirnfurtbot.hello
-      :world
+      iex> {:ok, pid} = Dornbirnfurtbot.start(1, 2)
+      iex>  = is_pid(pid)
+      iex> true == pid_returned
+      true
 
   """
-  def hello do
-    :world
+  def start(_type, _args) do
+    import Supervisor.Spec, warn: false
+
+    envport = System.get_env("PORT") || 5001
+
+    port =
+      case envport do
+        envport when is_binary(envport) -> envport |> String.to_integer()
+        _ -> envport
+      end
+
+    Logger.info("Started application")
+    Logger.info("Port assigned to #{port}")
+
+    children = [
+      Plug.Adapters.Cowboy.child_spec(:http, Dornbirnfurtbot.Router, [], port: port),
+      worker(Dornbirnfurtbot.Waterlevel, [])
+    ]
+
+    Supervisor.start_link(children, strategy: :one_for_one)
   end
 end
