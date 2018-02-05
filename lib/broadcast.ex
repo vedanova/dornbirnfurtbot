752ebbef86f177d50150c3d9fbfefff45bcc0668
Christoph Klocker <christoph@vedanova.com>
2018-02-05 11:44:06 +0100

    updated doc

--- lib/broadcast.ex
+++ lib/broadcast.ex
@@ -1,12 +1,10 @@
 defmodule Dornbirnfurtbot.Broadcast do
   @doc ~S"""
-  Parses the given `line` into a command.
+  Sends out message to broadcast receivers
 
   ## Examples
-
+  
       iex> Dornbirnfurtbot.Broadcast.broadcast "Test broadcast"
-      {:ok, {:create, "shopping"}}
-
   """
   def broadcast(message, notification_type \\ "NO_PUSH") do
     FacebookMessenger.Sender.text_broadcast(message, "TRANSPORTATION_UPDATE", notification_type)
