8dc566a77df6ba9ce4accc4fcdebf7fa75357833
Christoph Klocker <christoph@vedanova.com>
2018-02-13 23:13:15 +0100

    removed flood alerting based on water level.

--- lib/waterlevel.ex
+++ lib/waterlevel.ex
@@ -10,9 +10,9 @@ defmodule Dornbirnfurtbot.Waterlevel do
             state: :initialized
 
   # level guards
-  defguard alert_level(height) when height >= 3.5
-  defguard warn_level(height) when height < 3.5 and height >= 3.0
-  defguard below_warn_level(height) when height < 3.0
+  defguard warn_level(height) when height >= 3.0
+  @doc "Lower than warn level to make sure it will not go straight back up to warning"
+  defguard below_warn_level(height) when height < 2.8
   # gate state guards
   defguard is_closed(new_state) when new_state <= 0
   defguard is_open(new_state) when new_state > 0
@@ -50,38 +50,23 @@ defmodule Dornbirnfurtbot.Waterlevel do
 
   # ------ level states
 
-  # flodded && alarm sent already
-  def check(%{state: :flodded} = state_data, height) when alert_level(height) do
-    %{state_data | state: :flodded, waterlevel: height}
-  end
-
-  # flodded
-  def check(%{state: :warned} = state_data, height) when alert_level(height) do
-    message = "Die Furt ist gesperrt. Höhe: #{inspect(height)}"
-    @broadcaster.broadcast(message, "REGULAR")
-    %{state_data | state: :flodded, waterlevel: height}
-  end
-
   # warned, already sent
   def check(%{state: :warned} = state_data, height) when warn_level(height) do
     %{state_data | state: :warned, waterlevel: height}
   end
 
-  # flodded, returns back to warn level to not alarm twice
-  def check(%{state: :flodded} = state_data, height) when warn_level(height) do
-    message = "Die Furt wird wahrscheinlich bald wieder offen sein. Höhe: #{inspect(height)}"
-    @broadcaster.broadcast(message, "REGULAR")
-    %{state_data | state: :level_drop, waterlevel: height}
-  end
-
   # drop of waterlevel back to normal
-  def check(%{state: :level_drop} = state_data, height) when warn_level(height) do
-    %{state_data | state: :level_drop, waterlevel: height}
+  def check(%{state: :warned} = state_data, height) when below_warn_level(height) do
+    message = "Die Furt wird wahrscheinlich bald wieder offen sein. Höhe: #{inspect(height)}"
+    Logger.info("Broadcasting #{message}")
+    @broadcaster.broadcast(message, "REGULAR")
+    %{state_data | state: :normal, waterlevel: height}
   end
 
   # notify soon flodded
   def check(%{state: :normal} = state_data, height) when warn_level(height) do
     message = "Die Furt wird vielleicht bald geschlossen sein"
+    Logger.info("Broadcasting #{message}")
     @broadcaster.broadcast(message, "REGULAR")
     %{state_data | state: :warned, waterlevel: height}
   end
